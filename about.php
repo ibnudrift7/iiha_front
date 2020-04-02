dae40a83e7b60f6cd007e9fb5ebd4a17e037899b
messirichard <richardofay@gmail.com>
2020-04-03 00:19:07 +0700

    about

---
+++ about.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>About Us</title>
+	
+    <!-- Bootstrap core CSS -->
+    <link href="asset/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="asset/css/gallery.css" rel="stylesheet">
+    <link href="asset/css/about.css" rel="stylesheet">
+</head>
+<body>
+
+	<!-- start about	 -->
+	<section class="block-about loc-about">
+
+        <section class="about-blc-top-1 back-greywhite py-5">
+    		<div class="prelatife container">
+    			<div class="inner py-4 mt-4">
+                        <h4>“ Our vision is to become a well-recognized organization with excellent reputation in developing professional competency of industrial hygiene resources to protect worker’s health and safety ”</h4>
+                        <div class="py-4"></div>
+                        <div class="counter-about-top">
+                        <div class="row">
+                            <div class="col">
+                                <div class="text-block">
+                                <span>2016</span>
+                                <p>ANOH Affiliate Member</p>
+                                </div>
+                            </div>
+                            
+                            <div class="col">
+                                <div class="text-block">
+                                <span>2016</span>
+                                <p>ANOH Affiliate Member</p>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="text-block">
+                                <span>>4</span>
+                                <p>Members</p>
+                                </div>
+                            </div>
+
+                        </div>
+                        </div>
+    				
+
+    				<div class="clear"></div>
+    			</div>
+    		</div>
+        </section>
+
+        <section class="about-blc-content section-2 back-bluedark">
+            <div class="prelatife container">
+                <div class="inner pt-5">
+                    <h2>Who are we</h2>
+                    <div class="py-3"></div>
+                    <div class="row content-boxed">
+                        <div class="col-md-6">
+                            <div class="picture-abs"><img src="asset/images/banner-about-.jpg" alt="" class="img img-fluid"></div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="info">
+                                <p>The Indonesian Industrial Hygiene Association (IIHA) is a voluntary, non-profit, non-governmental professional credentialing organization.</p>
+                                <p>IIHA was established on the 12th January 2016 and officially approved by Ministry of Human Right and Law – Republic of Indonesia by the 10th February 2016 (approval letter No.: AHU-0023179.AH.01.07.TAHUN 2016).</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clear clearfix"></div>
+                </div>
+            </div>
+        </section>
+
+        <section class="about-blc-content section-3 back-grey py-5">
+            <div class="prelatife container">
+                <div class="inner">
+                    <h2>Our Aims</h2>
+                    <div class="py-4"></div>
+                    <div class="outer-box">
+                        <div class="row no-gutters content-boxed">
+                            <div class="col-md-7">
+                                <div class="picture-abs"><img src="https://placehold.it/466x270" alt="" class="img img-fluid"></div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="info bottoms">
+                                    <p>“Provide professional standards for its members on becoming a professional industrial hygienist in delivering services through integrity and professionalism”</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="py-5"></div>
+                        <div class="py-2"></div>
+
+                        <div class="row no-gutters content-boxed">
+                            <div class="col-md-4">
+                                <div class="info tops">
+                                    <p>“Increase the highest professional competency for its members in order to compete internationally in the field of Industrial Hygiene”</p>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="picture-abs"><img src="https://placehold.it/466x270" alt="" class="img img-fluid"></div>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+
+                    <div class="clear clearfix"></div>
+                </div>
+            </div>
+        </section>
+
+        <section class="about-blc-content section-4 back-white py-5">
+            <div class="prelatife container">
+                <div class="inner">
+                    <h2>Our Missions</h2>
+                    <div class="py-4"></div>
+                    
+                    <?php 
+                    $arr_mission = [
+                                        [
+                                            'icons'=>'',
+                                            'info'=>'Increase the highest professional competency for its members in order to compete internationally in the field of Industrial Hygiene.',
+                                        ],
+                                        [
+                                            'icons'=>'',
+                                            'info'=>'Increase the highest professional competency for its members in order to compete interna',
+                                        ],
+                                        [
+                                            'icons'=>'',
+                                            'info'=>'Increase the highest professional competency for its members in order.',
+                                        ],
+                                        [
+                                            'icons'=>'',
+                                            'info'=>'Increase the highest professional competency for its members in order to compete interna',
+                                        ],
+                                        [
+                                            'icons'=>'',
+                                            'info'=>'Increase the highest professional competency for its members in order.',
+                                        ],
+                                        [
+                                            'icons'=>'',
+                                            'info'=>'Increase the highest professional competency for its members in order to compete internationally in the field ',
+                                        ],
+                                    ];
+                    ?>
+                    <div class="boxed-list-mission">
+                        <div class="row">
+                            <?php foreach ($arr_mission as $key => $value): ?>
+                            <div class="col-md-4">
+                                <div class="box-item text-center">
+                                    <div class="pict"><img src="https://placehold.it/110x110" alt="" class="img img-fluid d-block mx-auto"></div>
+                                    <div class="info">
+                                        <p><?php echo $value['info'] ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php endforeach ?>
+                        </div>
+                    </div>
+
+                    <div class="clear clearfix"></div>
+                </div>
+            </div>
+        </section>
+
+
+	</section>
+	<!-- end about	 -->
+
+    <section class="outer-block-membership-belt py-5">
+        <div class="prelatife container">
+            <div class="inner-box">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4>Join IIHA <br>Membership</h4>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="#" class="btn btn-default btns_goto_member">Get Membership</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+	
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="asset/bootstrap/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
