3f4141f23f43c2775d15b8151df1a78a9c9af747
messirichard <richardofay@gmail.com>
2020-04-04 03:47:32 +0700

    updates ttm

---
+++ company_member.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Company member</title>
+	
+    <!-- Bootstrap core CSS -->
+    <link href="asset/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!-- Custom styles for this template -->
+    <link href="asset/css/newsletter.css" rel="stylesheet">
+    <link href="asset/css/member.css" rel="stylesheet">
+</head>
+<body>
+
+	<!-- start newsletter	 -->
+	<section class="block-newsletter blocks-company-member py-5">
+		<div class="prelatife container">
+			<div class="inners">			
+				<div class="inside mx-auto">
+
+					<div class="tops text-center">
+						<h1>Company Member</h1>
+						<div class="py-1"></div>
+						<span class="dates">Lorem ipsum dolor sit amet</span>
+						<div class="py-1"></div>
+					</div>
+
+					<div class="py-3 my-2"></div>
+
+					<div class="lists_company_members">
+						<div class="row justify-content-center">
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/VVLMgnz/Gagas.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">Labenviro</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/NxBd01G/Group-1968.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">AAS Laboratory</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/VVLMgnz/Gagas.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">Labenviro</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/NxBd01G/Group-1968.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">AAS Laboratory</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/VVLMgnz/Gagas.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">Labenviro</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/NxBd01G/Group-1968.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">AAS Laboratory</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/VVLMgnz/Gagas.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">Labenviro</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/NxBd01G/Group-1968.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">AAS Laboratory</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="item">
+									<div class="picture"><img src="https://i.ibb.co/VVLMgnz/Gagas.jpg" alt="" class="img img-fluid"></div>
+									<div class="info py-2 text-center">
+										<span class="d-block text-center">Labenviro</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="clear"></div>
+					</div>
+
+
+					<div class="clearfix"></div>
+				</div>
+				<div class="py-4"></div>
+
+				<div class="clear"></div>
+			</div>
+		</div>
+	</section>
+	<!-- end newsletter	 -->
+	
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="asset/bootstrap/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+
---
+++ organization.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Company Organization</title>
+	
+    <!-- Bootstrap core CSS -->
+    <link href="asset/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!-- Custom styles for this template -->
+    <link href="asset/css/newsletter.css" rel="stylesheet">
+    <link href="asset/css/organization.css" rel="stylesheet">
+</head>
+<body>
+
+	<!-- start newsletter	 -->
+	<section class="block-newsletter blocks-company-organization py-5">
+		<div class="prelatife container">
+			<div class="inners">			
+				<div class="inside mx-auto">
+
+					<div class="tops text-center">
+						<h1>Organizational Chart</h1>
+						<div class="py-1"></div>
+					</div>
+
+					<div class="py-3 my-2"></div>
+					<div class="picture_full"><img src="https://i.ibb.co/94qyn9W/Organizational-Chart-1.jpg" alt="" class="img img-fluid w-100"></div>
+
+					<div class="clearfix"></div>
+				</div>
+				<div class="py-3"></div>
+				<div class="clear"></div>
+			</div>
+		</div>
+
+		<div class="back-grey section2-organization py-5">
+			<div class="prelatife container">
+				<div class="inners mx-auto">
+					<h2 class="text-center">Board of Advisory</h2>
+					<div class="py-3 my-2"></div>
+					<div class="blocks-users">
+						<div class="row">
+							<div class="col-md-5 my-auto">
+								<p><strong>Chairman of IIHA BoA</strong></p>		
+							</div>
+							<div class="col-md-7">
+								<p><img src="https://i.ibb.co/Cz40JWF/Ellipse-71.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+							</div>
+						</div>
+					</div>
+					<div class="py-3"></div>
+					<div class="lines-grey"></div>
+					<div class="py-3"></div>
+
+					<div class="blocks-users">
+						<div class="row">
+							<div class="col-md-5 my-auto">
+								<p><strong>BoA Member</strong></p>		
+							</div>
+							<div class="col-md-7">
+								<p><img src="https://i.ibb.co/Cz40JWF/Ellipse-71.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+								<div class="py-2"></div>
+								<p><img src="https://i.ibb.co/jvqxwFD/Ellipse-72.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+								<div class="py-2"></div>
+								<p><img src="https://i.ibb.co/SwcQwsS/Ellipse-73.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+								<div class="py-2"></div>
+								<p><img src="https://i.ibb.co/mBYMtCM/Ellipse-74.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+								<div class="py-2"></div>
+								<p><img src="https://i.ibb.co/q5Z0GpL/Ellipse-75.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="clear"></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="back-blue-dark section2-organization py-5">
+			<div class="prelatife container">
+				<div class="inners mx-auto">
+					<h2 class="text-center">Board of Director</h2>
+					<div class="py-3 my-2"></div>
+					<div class="blocks-users">
+						<div class="row">
+							<div class="col-md-5 my-auto">
+								<p><strong>President of IIHA</strong></p>		
+							</div>
+							<div class="col-md-7">
+								<p><img src="https://i.ibb.co/Cz40JWF/Ellipse-71.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+							</div>
+						</div>
+					</div>
+					<div class="py-3"></div>
+					<div class="lines-grey"></div>
+					<div class="py-3"></div>
+
+					<div class="blocks-users">
+						<div class="row">
+							<div class="col-md-5 my-auto">
+								<p><strong>General Secretary of IIHA</strong></p>		
+							</div>
+							<div class="col-md-7">
+								<p><img src="https://i.ibb.co/Cz40JWF/Ellipse-71.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+								<div class="py-2"></div>
+								<p><img src="https://i.ibb.co/jvqxwFD/Ellipse-72.jpg" alt="" class="img img-fluid mr-3"> Dr. Ir. Sjahrul M. Nasri, M. SC (Hyg), HIU.</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="clear"></div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<!-- end newsletter	 -->
+	
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="asset/bootstrap/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+
