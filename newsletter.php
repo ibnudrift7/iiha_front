5b8aaff80550dea016176932326f862e8f59c7be
messirichard <richardofay@gmail.com>
2020-04-03 20:55:27 +0700

    process 22

---
+++ newsletter.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Newsletter</title>
+	
+    <!-- Bootstrap core CSS -->
+    <link href="asset/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="asset/css/newsletter.css" rel="stylesheet">
+</head>
+<body>
+
+	<!-- start newsletter	 -->
+	<section class="block-newsletter py-5">
+		<div class="prelatife container">
+			<div class="inners">			
+				<div class="inside mx-auto">
+
+					<div class="tops text-center">
+						<h1>Newsletter IIHA - Maret 2020</h1>
+						<div class="py-1"></div>
+						<span class="dates">March 23, 2019</span>
+						<div class="py-1"></div>
+					</div>
+
+					<div class="py-2 my-1"></div>
+					<div class="picture"><img src="https://i.ibb.co/s2yqR8s/banner-newsletter.jpg" alt="" class="img img-fluid"></div>
+					
+
+					<div class="bottom-description">
+						<p>As an industrial hygienist, you spend a lot of your time making data-driven decisions. Have you considered the impact of the traceability of the safety data sheets (SDSs) you use to protect your workers? </p>
+						<p>In this newsletter, we will talk about how to effectively make your data-driven descision with the consideration of the traceability of the safety data sheets (SDSs)  impact that we use for protecting our workers.</p>
+						<div class="py-2"></div>
+						<a href="#" class="btn btn-dark-purple big">Download Newsletter</a>
+					</div>
+
+					<div class="clearfix"></div>
+				</div>
+				<div class="py-4"></div>
+
+				<div class="social-media-boxed py-4">
+					<div class="  d-block mx-auto">
+						<h5>Share</h5>
+						<div class="clear clearfix"></div>
+						<ul class="list-inline">
+							<li class="list-inline-item">
+								<a class="i_fb" href=""><i class="fa fa-facebook"></i></a>
+							</li>
+							<li class="list-inline-item">
+								<a class="i_twitter" href=""><i class="fa fa-twitter"></i></a>
+							</li>
+							<li class="list-inline-item">
+								<a class="i_phone" href=""><i class="fa fa-phone"></i></a>
+							</li>
+							<li class="list-inline-item">
+								<a class="i_instagram" href=""><i class="fa fa-instagram"></i></a>
+							</li>
+							<li class="list-inline-item">
+								<a class="i_linkedin" href=""><i class="fa fa-linkedin"></i></a>
+							</li>
+							
+						</ul>
+						<div class="clear clearfix"></div>
+					</div>
+				</div>
+
+				<div class="clear"></div>
+			</div>
+		</div>
+	</section>
+	<!-- end newsletter	 -->
+	
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="asset/bootstrap/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+
