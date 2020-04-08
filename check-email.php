8d5952df1f7e73aa810483722b3f7045dc1095e5
messirichard <richardofay@gmail.com>
2020-04-08 14:34:35 +0700

    updates login - forgot - check email - false login

---
+++ check-email.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Forgot</title>
+	
+    <!-- Bootstrap core CSS -->
+    <link href="asset/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="asset/css/gallery.css" rel="stylesheet">
+    <link href="asset/css/login.css" rel="stylesheet">
+</head>
+<body>
+
+	<!-- start Login	 -->
+	<div class="py-5"></div>
+	<div class="prelatife container login-wrapper">
+		
+		<section class="outer-block-login-section back-white">
+			<div class="inner-wrapper">
+				<div class="row no-gutters">
+					<div class="col-md-6 my-auto">
+						<div class="full-banner"><img src="https://i.ibb.co/MCnV0nz/Rectangle-102.jpg" alt="" class="img-fluid w-100"></div>
+					</div>
+					<div class="col-md-6 prelatife">
+						<div class="right-form forgot-password">
+							<div class="tn_box_close"><a href="#"><img src="https://i.ibb.co/mB9xRyL/close-vector.png" alt="" class="img img-fluid"></a></div>
+
+							<div class="block-inner-top py-5 p-3">
+								<div class="tops_text mx-4">
+									<h3>Check your email</h3>
+									<p>We have sent you an email. It contains link for you to reset your password. Please check it.</p>
+								</div>
+								<div class="card mx-4">
+									<article class="card-body">
+										<form method="post" action="#">
+										    <div class="form-group">
+										        <a href="btn btn-primary btn-block">Back to Login</a>
+										    </div> <!-- form-group// -->                                    
+										</form>
+									</article>
+								</div>
+							</div>
+							<div class="clear clearfix"></div>
+
+
+							<div class="text-center texts-bottoms">
+								<p>&copy; 2020 IHHA. All rights reserved. <br>
+									By signing up, I agree to our <a href="#">Term of Service</a></p>
+							</div>
+
+
+							<div class="clear"></div>
+						</div>
+					</div>
+				</div>		
+			</div>
+		</section>
+
+	</div>
+	<!-- end Login	 -->
+	
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="asset/bootstrap/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+
